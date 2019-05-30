2b3126f59cd518ec16fdd0f60e5936a657861455
Max Bruckner <max@maxbruckner.de>
2019-05-31 00:32:41 +0200

    dbmigrate: Use new library with new Migration struct

--- dbmigrate/src/cmd.rs
+++ dbmigrate/src/cmd.rs
@@ -1,23 +1,21 @@
 use std::path::Path;
 use std::time::Instant;
 
-use dbmigrate_lib::{Driver, create_migration, Migrations, MigrationFile, Direction};
+use dbmigrate_lib::{Driver, create_migration, Migrations, Migration, Direction};
 use print;
 use errors::{Result};
 
 
 // Does the whole migration thingy, along with timing and handling errors
-fn migrate(driver: &dyn Driver, migration_file: &MigrationFile) -> Result<()> {
-    let direction = &migration_file.direction;
-    println!("Running {} migration #{}: {}", direction.to_string(), migration_file.number, migration_file.name);
+fn migrate(driver: &dyn Driver, migration: &Migration, number: u16, direction: Direction) -> Result<()> {
+    println!("Running {} migration #{}: {}", direction.to_string(), number, migration.name);
 
     let start = Instant::now();
 
-    let number = match direction {
-        Direction::Up => migration_file.number,
-        Direction::Down => migration_file.number - 1,
+    let (number, content) = match direction {
+        Direction::Up => (number, migration.up.to_owned()),
+        Direction::Down => (number - 1, migration.down.to_owned()),
     };
-    let content = migration_file.content.as_ref().unwrap().to_owned();
 
     driver.migrate(content, number)?;
 
@@ -27,7 +25,7 @@ fn migrate(driver: &dyn Driver, migration_file: &MigrationFile) -> Result<()> {
 }
 
 pub fn create(migration_files: &Migrations, path: &Path, slug: &str) -> Result<()> {
-    let current_number = migration_files.keys().cloned().max().unwrap_or(0i32);
+    let current_number = migration_files.keys().cloned().max().unwrap_or(0u16);
     let number = current_number + 1;
     match create_migration(path, slug, number) {
         Err(e) => Err(e.into()),
@@ -39,86 +37,82 @@ pub fn create(migration_files: &Migrations, path: &Path, slug: &str) -> Result<(
 }
 
 
-pub fn status(driver: Box<Driver>, migration_files: &Migrations) -> Result<()> {
+pub fn status(driver: Box<Driver>, migrations: &Migrations) -> Result<()> {
     let current = driver.get_current_number();
+
     if current == 0 {
         print::success("No migration has been ran");
     }
-    for (number, migration) in migration_files.iter() {
-        let mig_file = migration.up.as_ref().unwrap();
+    for (number, migration) in migrations.iter() {
+        let name = &migration.name;
         if number == &current {
-            print::success(&format!("{} - {} (current)", mig_file.number, mig_file.name));
+            print::success(&format!("{} - {} (current)", number, name));
         } else {
-            println!("{} - {}", mig_file.number, mig_file.name);
+            println!("{} - {}", number, name);
         }
     }
     Ok(())
 }
 
 
-pub fn up(driver: Box<Driver>, migration_files: &Migrations) -> Result<()> {
+pub fn up(driver: Box<Driver>, migrations: &Migrations) -> Result<()> {
     let current = driver.get_current_number();
-    let max = migration_files.keys().max().unwrap();
+    let max = migrations.keys().max().unwrap();
     if current == *max {
         print::success("Migrations are up-to-date");
         return Ok(());
     }
 
-    for (number, migration) in migration_files.iter() {
+    for (number, migration) in migrations.iter() {
         if number > &current {
-            let mig_file = migration.up.as_ref().unwrap();
-            migrate(driver.as_ref(), mig_file)?;
+            migrate(driver.as_ref(), migration, *number, Direction::Up)?;
         }
     }
     Ok(())
 }
 
-pub fn down(driver: Box<Driver>, migration_files: &Migrations) -> Result<()> {
+pub fn down(driver: Box<Driver>, migrations: &Migrations) -> Result<()> {
     let current = driver.get_current_number();
     if current == 0 {
         print::success("No down migrations to run");
         return Ok(());
     }
 
-    let numbers = migration_files.keys()
+    let numbers = migrations.keys()
         .rev()
         .cloned()
         .filter(|i| i <= &current);
 
     for number in numbers {
-        let migration = migration_files.get(&number).unwrap();
-        let mig_file = migration.down.as_ref().unwrap();
-        migrate(driver.as_ref(), mig_file)?;
+        let migration = migrations.get(&number).unwrap();
+        migrate(driver.as_ref(), migration, number, Direction::Down)?;
     }
     Ok(())
 }
 
-pub fn redo(driver: Box<Driver>, migration_files: &Migrations) -> Result<()> {
+pub fn redo(driver: Box<Driver>, migrations: &Migrations) -> Result<()> {
     let current = driver.get_current_number();
     if current == 0 {
         print::success("No migration to redo");
         return Ok(());
     }
-    let migration = migration_files.get(&current).unwrap();
+    let migration = migrations.get(&current).unwrap();
 
-    let down_file = migration.down.as_ref().unwrap();
-    let up_file = migration.up.as_ref().unwrap();
 
-    migrate(driver.as_ref(), down_file)?;
-    migrate(driver.as_ref(), up_file)?;
+    migrate(driver.as_ref(), migration, current, Direction::Down)?;
+    migrate(driver.as_ref(), migration, current, Direction::Up)?;
     Ok(())
 }
 
 
-pub fn revert(driver: Box<Driver>, migration_files: &Migrations) -> Result<()> {
+pub fn revert(driver: Box<Driver>, migrations: &Migrations) -> Result<()> {
     let current = driver.get_current_number();
     if current == 0 {
         print::success("No migration to revert");
         return Ok(());
     }
-    let migration = migration_files.get(&current).unwrap();
-    let down_file = migration.down.as_ref().unwrap();
+    let migration = migrations.get(&current).unwrap();
 
-    migrate(driver.as_ref(), down_file)?;
+    migrate(driver.as_ref(), migration, current, Direction::Down)?;
     Ok(())
 }
